f32cfedc32345e400ed66f4731df397c8ce250b9
kimhoho8601-alt <kimhoho8601@gmail.com>
2026-08-31 15:49:47 +0900

    Connect teamleader workspace to Supabase live backend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TEAMLEADER_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://ynqzzdelgaivuriyxurq.supabase.co',
+  supabaseAnonKey: 'sb_publishable_iJdKCuA7_lr98TkFFG1PvQ_A5RD9EAa'
 };
